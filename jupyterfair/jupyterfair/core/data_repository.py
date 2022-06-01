5aa61260ecda2b5bf657d476f7f3e81787e2b873
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-06-01 15:25:00 +0200

    add CRUD methods to abstract class DataRepositoryAPI

--- jupyterfair/jupyterfair/core/data_repository.py
+++ jupyterfair/jupyterfair/core/data_repository.py
@@ -5,14 +5,35 @@ Abstractions for Data Repository
 from abc import ABC, abstractmethod
 
 
-class DataRepository(ABC):
-    """ Abstaract class for research data repositories"""
+class DataRepositoryAPI(ABC):
+    """ Abstaract class for research data repository APIs"""
     
     @abstractmethod
-    def create_entry(self, **argv):
-        """Creates a new data entry in the data repository"""
+    def create_item(self, **argv):
+        """
+        Creates a new item in the user account attached to a research data repository. 
+        The type of item(s) to be created depends on the iplementation.
+        """
         pass
 
     @abstractmethod
-    def upload_data(self, **argv):
-        """Uploads files to the data repository"""
+    def get_item(self, **argv):
+        """
+        Retrieves one or more items in the user account attached to the reserch data repository.
+        """
+        pass
+
+    @abstractmethod
+    def update_item(self, **argv):
+        """
+        Updates metadata of an existing item in the user account attached to the research data
+        repository.
+        """
+        pass
+
+    @abstractmethod
+    def delete_item(self, **argv):
+        """
+        deletes an existing item in the user account attached to the research data
+        repository.
+        """
