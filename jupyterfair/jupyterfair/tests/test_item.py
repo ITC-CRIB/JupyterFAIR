74dcb27467f2cbed684d502ad2aa8c9d6e91be72
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-06-02 12:27:35 +0200

    add scafolding for testing Item class

---
+++ jupyterfair/jupyterfair/tests/test_item.py
@@ -0,0 +1,27 @@
+"""
+Tests for the Item class
+"""
+import unittest
+from jupyterfair.core.item import Item
+
+class TestItem(unittest.TestCase):
+
+    # fixtures
+    def setUp(self) -> None:
+
+        pass
+        return super().setUp()
+
+    def test_(self):
+        """ Test if the GET returns retuns a Requests response object and a success status code: 200"""
+        pass
+        
+
+    # clean up action
+    def tearDown(self) -> None:
+        pass
+        return super().tearDown()
+
+
+if __name__ == '__main__':
+    unittest.main()
